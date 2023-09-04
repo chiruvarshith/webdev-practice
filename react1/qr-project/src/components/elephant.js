7a37f76d7677d3760f6e4540da15d884a8d61c79
jashwanth0712 <jashwanth0712@gmail.com>
2023-09-04 08:39:27 +0530

    updated image styling

--- react1/qr-project/src/components/elephant.js
+++ react1/qr-project/src/components/elephant.js
@@ -1,8 +1,21 @@
 //
 import React from 'react';
-  
+import ima from "../assets/images/elephant.png"
 function Elephant (){
-    return <h1>Elephant</h1>
+    return (
+        <div className='m-5 '>
+            <div class="relative">
+                <img class="h-auto max-w-full rounded-xl" src={ima} alt="image description"/>
+                <div class="absolute inset-x-0 bottom-0 h-1/2 bg-gradient-to-t from-black to-transparent flex items-center justify-center">
+                    <h2 class="text-white font-semibold">elephant</h2>
+                </div>
+            </div>
+        </div>
+
+
+        
+
+    )
 }
   
 export default Elephant;
