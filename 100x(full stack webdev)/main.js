6ae95c4e2140794608f4784f30fa2a15e8f564ed
chiruvarshith <chiruvarshithpeddisetty@gmail.com>
2024-04-22 22:30:19 +0530

    basic java programm

---
+++ 100x(full stack webdev)/main.js
@@ -0,0 +1,24 @@
+let aname = "chiru";
+let age=18;
+let job="student";
+let ismarried=false;
+let enternum=100;
+let answer=0;
+
+console.log("name of the person is " + aname + " and his age is: " + age + " he is a " + job);
+
+if (ismarried==true){
+    console.log(aname + " is married");
+}
+
+else{
+    console.log(aname + " is not married");
+}
+
+
+for(let i=0;i<=enternum;i++){
+    answer=answer+i;
+}
+
+console.log(answer);
+
